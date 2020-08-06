0d59890c6817c03ccb1a28b69a50f231062371f7
Darwin Gualotuña <daosgc@gmail.com>
2020-08-06 12:49:11 -0500

    Implement styled component in NewTodoForm component

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -2,21 +2,47 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
 import { createTodo } from './actions'
-import './NewTodoForm.css'
+import styled from 'styled-components'
+
+const TodoForm = styled.div`
+  padding: 16px 0;
+  text-align: center;
+  box-shadow: 0 4px 8px #ddd;
+`
+
+const TodoInput = styled.input`
+  font-size: 16px;
+  padding: 8px;
+  border: none;
+  border-bottom: 2px solid #ddd;
+  width: 70%;
+  outline: none;
+`
+
+const AddButton = styled.button`
+  font-size: 16px;
+  padding: 8px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  margin-left: 8px;
+  width: 20%;
+  background-color: #3892e5;
+  color: #fff;
+`
 
 const NewTodoForm = ({ todos, onCreatePressed }) => {
   const [inputValue, setInputValue] = useState('')
 
   return (
-    <div className="new-todo-form">
-      <input
-        className="new-todo-input"
+    <TodoForm>
+      <TodoInput
         type="text"
         placeholder="Type your todo here"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button
+      <AddButton
         onClick={() => {
           const isInvalidText = inputValue === '' || todos.some((todo) => todo.text === inputValue)
           if (!isInvalidText) {
@@ -24,11 +50,10 @@ const NewTodoForm = ({ todos, onCreatePressed }) => {
             setInputValue('')
           }
         }}
-        className="new-todo-button"
       >
         Create Todo
-      </button>
-    </div>
+      </AddButton>
+    </TodoForm>
   )
 }
 
